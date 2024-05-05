a2f0a2b49381a56a8ab81d82655bb4e8579e47c4
Rahul Kumar <130215217+rahul-1105@users.noreply.github.com>
2024-05-05 11:55:55 +0530

    Update server code to use ES modules syntax

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express');
+import express from "express";
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("Wohoooo!! Server is running")
-})
+app.get("/", (req, res) => {
+  res.send("Wohoooo!! Server is running");
+});
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-})
+  console.log("Server is running on port 3000");
+});
